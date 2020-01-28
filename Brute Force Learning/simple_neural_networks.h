e25c8b1f40e1c10eff15449ce4db6a144f6ef080
PratikhyaManas Behera <35187384+PratikhyaManas@users.noreply.github.com>
2020-01-28 20:09:04 +0100

    Update simple_neural_networks.h

--- Brute Force Learning/simple_neural_networks.h
+++ Brute Force Learning/simple_neural_networks.h
@@ -14,29 +14,28 @@ void single_input_multiple_output_nn(	double input_scalar,
 																			double VECTOR_LEN);
 																			
 void multiple_inputs_multiple_outputs_nn(double * input_vector,
-																	uint32_t INPUT_LEN,
-																	double * output_vector,
-																	uint32_t OUTPUT_LEN,
-																	double weights_matrix[OUTPUT_LEN][INPUT_LEN]);
+					 uint32_t INPUT_LEN,
+					 double * output_vector,
+					 uint32_t OUTPUT_LEN,
+					 double weights_matrix[OUTPUT_LEN][INPUT_LEN]);
 	
 																	
 void hidden_nn( double *input_vector,
-								uint32_t INPUT_LEN,
-								uint32_t HIDDEN_LEN,
+		uint32_t INPUT_LEN,
+		uint32_t HIDDEN_LEN,
                 double in_to_hid_weights[HIDDEN_LEN][INPUT_LEN],
-								uint32_t OUTPUT_LEN,
-								double hid_to_out_weights[OUTPUT_LEN][HIDDEN_LEN],
-								double *output_vector
-							);
+		uint32_t OUTPUT_LEN,
+		double hid_to_out_weights[OUTPUT_LEN][HIDDEN_LEN],
+		double *output_vector);
 								
 	double  find_error_smpl( double yhat, double y);
 	double find_error_(double input, double weight, double expected_value);
 
 
 void brute_force_learning( double input,
-													 double weight,
-													 double expected_value,
-													 double step_amount,
-													 uint32_t itr);								
+			   double weight,
+			   double expected_value,
+			   double step_amount,
+			   uint32_t itr);								
 
 #endif
